6eabfab90c9781cbd78a26b203115ccc96c2fd1f
Restyled.io <commits@restyled.io>
2023-11-01 16:14:56 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Switch from "@mui/material/Switch";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 import BottomUpScroller from "./components/BottomUp";
 import Contact from "./components/Contact";
@@ -9,13 +9,16 @@ import Projects from "./components/Projects";
 import TechStack from "./components/TechStack";
 import baseTheme from "./themes/base";
 import darkTheme from "./themes/dark";
-import {applyTheme} from "./themes/utils";
+import { applyTheme } from "./themes/utils";
 
 function App() {
-  useEffect(() => { applyTheme(baseTheme); }, []);
+  useEffect(() => {
+    applyTheme(baseTheme);
+  }, []);
 
-  const handleChange =
-      (evt) => { applyTheme(evt.target.checked ? darkTheme : baseTheme); };
+  const handleChange = (evt) => {
+    applyTheme(evt.target.checked ? darkTheme : baseTheme);
+  };
 
   return (
     <>
